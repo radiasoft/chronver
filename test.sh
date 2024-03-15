7aed7d8e8930bcd5baf74a1374fde0beaf75819e
robnagler <github@q33.us>
2024-03-15 17:57:16 +0000

    work around https://github.com/radiasoft/pykern/issues/453

--- test.sh
+++ test.sh
@@ -13,6 +13,9 @@ test_main() {
     if grep -r -i pykern rschronver; then
         test_err "rschronver python files may not use/mention pykern"
     fi
+    # workaround https://github.com/radiasoft/pykern/issues/453
+    trap 'rm -f setup.py' EXIT
+    touch setup.py
     pykern ci run
 }
 
